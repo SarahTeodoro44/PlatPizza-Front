64b3a4b523ffa226351f8d8615c5bfe1ea438b22
Debora <debora.ramos.2@outlook.com>
2021-09-15 20:18:41 -0300

    pagina do footer

---
+++ src/components/footer/footer.jsx
@@ -0,0 +1,37 @@
+import './footer.css';
+import App from '../../assets/icons/app.png';
+import Entrega from '../../assets/icons/entrega.png';
+import Whatsapp from '../../assets/icons/whatsapp.png';
+import Twitter from '../../assets/icons/twitter.png';
+import Facebook from '../../assets/icons/facebook.png';
+import Instagram from '../../assets/icons/instagram.png'
+
+export function Footer() {
+    return (
+        <footer> <br></br> <br></br> <br></br> <br></br> <br></br> <br></br>
+            <div class="flex-container02 border-bottom-white02">
+                <div class="container">
+                    <div class="row">
+                        <div class="col">
+                            <div class="icons-nav"><img width="135" src={App }/></div>
+                            <p>Peça Pelo APP</p>
+                        </div>
+                        <div class="col">
+                            <div class="icons-nav"><img width="135" src={Entrega }/></div>
+                            <p>Fique tranquilo no conforto de sua casa, que nós entregamos</p>
+                        </div>
+                        <div class="col-6">
+                            <p>Nossas Redes Sociais</p>
+                            <div class="social-midias">
+                                <div class="icons-nav"><img width="135" src={Whatsapp }/></div>
+                                <div class="icons-nav"><img width="135" src={Twitter }/></div>
+                                <div class="icons-nav"><img width="135" src={Facebook }/></div>
+                                <div class="icons-nav"><img width="135" src={Instagram }/></div> 
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import { Header } from "../../components/header/header";
+import { Footer } from "../../components/footer/footer";
 
 export function Home() {
     return (
         <div>
             <div className="container">
                 <Header />
+                <Footer />
             </div>
         </div>
     )
