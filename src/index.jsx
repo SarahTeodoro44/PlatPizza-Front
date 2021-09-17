ddc382065165b6bc1c5cc03254f2a6ff4609156d
henriquezani <henrique.zani@hotmail.com>
2021-09-17 13:45:22 -0300

    Slide criado na página home

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import 'jquery/dist/jquery.min.js'
-import 'bootstrap/dist/js/bootstrap.bundle.js'
+import 'popper.js/dist/popper.js'
+import 'bootstrap/dist/js/bootstrap.js'
 import './styles/global.css';
 
 
