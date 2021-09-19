0197c8ba784ce49a035f4f70f3fc5b5b764cd26a
lucaoliver <lucasoliveiraahh@gmail.com>
2021-09-19 15:47:49 -0300

    bug ao pesquisar

--- src/components/SearchPage.jsx
+++ src/components/SearchPage.jsx
@@ -8,7 +8,6 @@ const SearchBar = ({keyword,setKeyword}) => {
      key="random1"
      value={keyword}
      placeholder={"Digite oque deseja"}
-     onChange={(e) => setKeyword(e.target.value)}
     />
   );
 }
