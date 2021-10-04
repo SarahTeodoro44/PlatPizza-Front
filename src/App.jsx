6747ede47437fba944fd13a925bdbed3eb19d7b5
Débora Ramos <debora.ramos.2@outlook.com>
2021-10-04 18:17:25 -0300

    add pagina

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Route, Switch, Link} from "react-router-dom";
 import { Home } from "./pages/Home/Home";
 import { Login } from "./pages/Login/Login";
 import { Contato } from "./pages/Contato/Contato";
+import { Cadastro } from "./pages/Cadastro/Cadastro";
 
 
 
@@ -13,6 +14,7 @@ function App() {
       <Route exact path="/home" component={Home} />
       <Route exact path="/login" component={Login} />
       <Route exact path="/contato" component={Contato} />
+      <Route exact path="/cadastro" component={Cadastro} />
     </Switch>
   </Router>
   );
