61867dc44587399514a570aa0b1b4b80003fe2ec
Débora Ramos <debora.ramos.2@outlook.com>
2021-10-04 18:51:33 -0300

    conteudo da pagina cadastro, estilização e adicionando fonte Poppins

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -1,3 +1,24 @@
 import React from "react";
 import './Cadastro.css';
+import BannerLogin from '../../assets/images/banner_login.png';
+import Et from '../../assets/icons/LogoEt01.png';
+
+
+export function Cadastro() {
+    return (
+        <main class="cadastro">
+            <div>
+                <img class="et" src={Et} alt="logo" />
+                <img class="banner-login"  src={ BannerLogin} alt="banner"/>
+                <div class="cadastro-area">
+                    <input type="email" class="form-email" placeholder="Email"/>
+                    <input type="password" class="form-senha" placeholder="Senha"/>
+                    <input type="password" class="form-confirma-senha" placeholder="Confirmação Senha"/>
+                    <button class="btn">Finalizar</button>
+                    <p class="voltar">Voltar</p>
+                </div> 
+            </div>
+        </main>
+    )
+}
 
