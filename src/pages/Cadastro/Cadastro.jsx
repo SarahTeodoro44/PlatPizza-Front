a91ffa4b91ca762f7938e8abefa71cbc423415e5
Débora Ramos <debora.ramos.2@outlook.com>
2021-10-04 22:03:49 -0300

    modificações

--- src/pages/Cadastro/Cadastro.jsx
+++ src/pages/Cadastro/Cadastro.jsx
@@ -11,11 +11,16 @@ export function Cadastro() {
                 <img class="et" src={Et} alt="logo" />
                 <img class="banner-login"  src={ BannerLogin} alt="banner"/>
                 <div class="cadastro-area">
-                    <input type="email" class="form-email" placeholder="Email"/>
-                    <input type="password" class="form-senha" placeholder="Senha"/>
-                    <input type="password" class="form-confirma-senha" placeholder="Confirmação Senha"/>
-                    <button class="btn">Finalizar</button>
-                    <p class="voltar">Voltar</p>
+                    <div class="info">
+                        <p>Digite seus Dados:</p>
+                    </div>
+                    <div class="cadastro-area2">
+                        <input type="email" class="form-email" placeholder="Email"/>
+                        <input type="password" class="form-senha" placeholder="Senha"/>
+                        <input type="password" class="form-confirma-senha" placeholder="Confirmação Senha"/>
+                        <button class="btn">Finalizar</button>
+                        <a class="voltar" href="#"><p>Voltar</p></a>
+                    </div>
                 </div> 
             </div>
         </main>
