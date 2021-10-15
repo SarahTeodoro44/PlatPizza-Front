f11f86d290409cc8a5aa921a544c66a477e0e98a
Débora Ramos <debora.ramos.2@outlook.com>
2021-10-15 12:28:47 -0300

    alteração na frase

--- src/pages/PagErro/PagErro.jsx
+++ src/pages/PagErro/PagErro.jsx
@@ -8,12 +8,12 @@ export function PagErro() {
         <main class="pagerro">
             <section class="container center">
                 <div class="item-v">4</div>
-                <div class="item"><img class="item" src={Et} alt="logo" width="200px" /></div>
+                <div class="item"><img class="item" src={Et} alt="logo" width="245px" /></div>
                 <div class="item">4</div>
             </section>
 
             <div class="frase">
-                    <p>Perdido no espaço</p>
+                    <p class="two-colors">Perdido no espaço</p>
             </div>
         </main>
     )
