4fe00054641019879cb0538b08d8f12721d4f8d3
Débora Ramos <debora.ramos.2@outlook.com>
2021-10-15 14:03:43 -0300

    teste

--- src/pages/PagErro/PagErro.jsx
+++ src/pages/PagErro/PagErro.jsx
@@ -6,12 +6,13 @@ import Et from '../../assets/icons/LogoEt01.png';
 export function PagErro() {
     return (
         <main class="pagerro">
-            <section class="container center">
-                <div class="item-v">4</div>
-                <div class="item"><img class="item" src={Et} alt="logo" width="245px" /></div>
-                <div class="item">4</div>
-            </section>
-
+            <div class="container center">
+                <div class="conteudo">
+                    <div class="item-v">4</div>
+                    <div class="item"><img class="item" src={Et} alt="logo" width="245px" /></div>
+                    <div class="item">4</div>
+                </div>
+            </div>
             <div class="frase">
                     <p class="two-colors">Perdido no espaço</p>
             </div>
